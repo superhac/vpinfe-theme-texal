e5944ebd4c53405486ce5679fa277177cb3b8d5a
superhac <superhac007@gmail.com>
2026-03-26 17:57:29 -0500

    support muting audio

--- theme.js
+++ theme.js
@@ -18,101 +18,6 @@ let selectorFrameTimeline = null;
 
 const mediaPreloadCache = new Map();
 
-const tableAudio = {
-    audio: Object.assign(new Audio(), { loop: true }),
-    fadeId: null,
-    fadeDuration: 500,
-    maxVolume: 0.8,
-    currentUrl: null,
-
-    play(url, retries = 3) {
-        if (!url) {
-            this.stop();
-            return;
-        }
-        if (this.currentUrl === url && !this.audio.paused) return;
-
-        const audio = this.audio;
-        clearInterval(this.fadeId);
-        audio.pause();
-        audio.volume = 0;
-        audio.src = url;
-        this.currentUrl = url;
-
-        audio.play().then(() => {
-            if (this.currentUrl === url) this._fade(0, this.maxVolume);
-        }).catch((e) => {
-            if (e.name === "NotAllowedError") {
-                this._retries = retries;
-                this._triggerWhenReady(url);
-            } else if (retries > 0 && this.currentUrl === url) {
-                setTimeout(() => this.play(url, retries - 1), 1000);
-            }
-        });
-    },
-
-    _triggerWhenReady(url) {
-        if (this.currentUrl !== url) return;
-        if (this.audio.readyState >= 2) {
-            vpin.call("trigger_audio_play").catch(() => {});
-        } else {
-            this.audio.addEventListener("canplay", () => {
-                if (this.currentUrl === url) {
-                    vpin.call("trigger_audio_play").catch(() => {});
-                }
-            }, { once: true });
-        }
-    },
-
-    _resumePlay() {
-        const url = this.currentUrl;
-        const retries = this._retries || 0;
-        if (!url) return;
-        this.audio.play().then(() => {
-            if (this.currentUrl === url) this._fade(0, this.maxVolume);
-        }).catch(() => {
-            if (retries > 0 && this.currentUrl === url) {
-                this._retries = retries - 1;
-                setTimeout(() => this._triggerWhenReady(url), 500);
-            }
-        });
-    },
-
-    stop() {
-        if (this.audio && !this.audio.paused) {
-            this._fade(this.audio.volume, 0, () => {
-                this.audio.pause();
-                this.currentUrl = null;
-            });
-        } else {
-            clearInterval(this.fadeId);
-            this.currentUrl = null;
-        }
-    },
-
-    _fade(from, to, onComplete) {
-        clearInterval(this.fadeId);
-        const audio = this.audio;
-        if (!audio) {
-            if (onComplete) onComplete();
-            return;
-        }
-        audio.volume = from;
-        const steps = this.fadeDuration / 20;
-        const delta = (to - from) / steps;
-        this.fadeId = setInterval(() => {
-            const next = audio.volume + delta;
-            if ((delta > 0 && next >= to) || (delta < 0 && next <= to) || delta === 0) {
-                audio.volume = to;
-                clearInterval(this.fadeId);
-                if (onComplete) onComplete();
-            } else {
-                audio.volume = next;
-            }
-        }, 20);
-    }
-};
-
 const vpin = new VPinFECore();
 vpin.init();
 window.vpin = vpin;
@@ -134,20 +39,17 @@ async function receiveEvent(message) {
     if (message.type === "TableIndexUpdate") {
         currentTableIndex = message.index;
         updateScreen();
-    } else if (message.type === "TableLaunching") {
-        tableAudio.stop();
+    }
+    else if (message.type === "TableLaunching") {
         fadeOut();
     } else if (message.type === "TableLaunchComplete") {
         fadeIn();
-        if (windowName === "table") tableAudio.play(vpin.getAudioURL(currentTableIndex));
     } else if (message.type === "RemoteLaunching") {
-        tableAudio.stop();
         showRemoteLaunchOverlay(message.table_name);
         fadeOut();
     } else if (message.type === "RemoteLaunchComplete") {
         hideRemoteLaunchOverlay();
         fadeIn();
-        if (windowName === "table") tableAudio.play(vpin.getAudioURL(currentTableIndex));
     } else if (message.type === "TableDataChange") {
         currentTableIndex = message.index;
         updateScreen();
@@ -175,7 +77,7 @@ async function handleInput(input) {
             });
             break;
         case "joyselect":
-            tableAudio.stop();
+            vpin.stopTableAudio();
             vpin.sendMessageToAllWindows({ type: "TableLaunching" });
             await fadeOut();
             await vpin.launchTable(currentTableIndex);
@@ -188,7 +90,7 @@ async function handleInput(input) {
 function updateScreen() {
     if (windowName === "table") {
         updateTableWindow();
-        tableAudio.play(vpin.getAudioURL(currentTableIndex));
+        vpin.playTableAudio(currentTableIndex);
         preloadNearbyMedia();
     } else if (windowName === "bg") {
         updateBGWindow();
